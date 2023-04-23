985f38440be27b1a31dfaf813f69505d3827bc34
Dichik <omeluan.dima@gmail.com>
2023-04-23 18:51:14 +0300

    Create Linear Layer

--- src/main/java/org/example/entity/torch/Linear.java
+++ src/main/java/org/example/entity/torch/Linear.java
@@ -5,13 +5,29 @@ public class Linear {
     private final int inputDim;
     private final int outputDim;
 
+    private Tensor weights;
+    private Tensor bias;
+
     public Linear(int inputDim, int outputDim) {
         this.inputDim = inputDim;
         this.outputDim = outputDim;
+
+        this.weights = new Tensor(outputDim, inputDim, true);
+        this.bias = new Tensor(outputDim, 1, true);
     }
 
-    void calculate(int[][] x) {
-        // TODO something useful
+    Tensor forward(Tensor input) {
+        Shape shape = input.getShape();
+        if (shape.getWidth() != inputDim) {
+            throw new RuntimeException("error...");
+        }
+
+        Tensor output = multiply(input, this.weights.T());
+        output.add(bias);
+        return output;
     }
 
+    public Tensor multiply(Tensor a, Tensor b) {
+        return null;
+    }
 }
