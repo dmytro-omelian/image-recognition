8a4e84d2df8580c793a65ab02fe81bc4ba034a74
Dichik <omeluan.dima@gmail.com>
2023-04-24 10:36:31 +0300

    Improve Tensor class and what's going on in Main.java 😰

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,10 @@ package org.example;
 
 import org.example.entity.data.DataLoader;
 import org.example.entity.data.Dataset;
+import org.example.entity.torch.Linear;
+import org.example.entity.torch.Loss;
 import org.example.neural_network.LogisticRegression;
-import org.example.optimizer.CrossEntropyLoss;
-import org.example.optimizer.SGD;
+import org.example.entity.torch.SGD;
 import org.example.preprocessing.PreprocessService;
 
 
@@ -36,27 +37,51 @@ public class Main {
         int outputDim = 10;
         LogisticRegression model = new LogisticRegression(inputDim, outputDim);
 
-        var error = new CrossEntropyLoss();
-        var optimizer = new SGD();
+//        var error = new CrossEntropyLoss();
+//        var optimizer = new SGD();
+//
+//        int numberOfEpochs = 10; // FIXME
+//
+//        for (int epoch = 0; epoch < numberOfEpochs; ++epoch) {
+//
+//            for (var batch : trainLoader.getBatches()) {
+//
+////                var images = batch.
+//
+////                optimizer.zero_grad();
+//
+////                var outputs = model.forward();
+//
+////                var loss = error.calculate(outputs, targets)
+//
+////                optimizer.step();
+//            }
+//
+//        }
+        double[][] input = {{0.1, 0.2, 0.3}, {0.4, 0.5, 0.6}, {0.7, 0.8, 0.9}};
+        double[][] target = {{0.3, 0.5}, {0.7, 0.2}, {0.1, 0.8}};
 
-        int numberOfEpochs = 10; // FIXME
+        Linear linear = new Linear(3, 2);
 
-        for (int epoch = 0; epoch < numberOfEpochs; ++epoch) {
+        Loss loss = new Loss();
 
-            for (var batch : trainLoader.getBatches()) {
+        double learningRate = 0.01;
+        SGD optimizer = new SGD(linear, learningRate);
 
-//                var images = batch.
+        int numEpochs = 1000;
+        for (int epoch = 1; epoch <= numEpochs; epoch++) {
+            double[][] output = linear.forward(input);
+            double[][] gradOutput = loss.backward(output, target);
+            double[][] gradInput = linear.backward(input, gradOutput, learningRate);
+            optimizer.step();
 
-//                optimizer.zero_grad();
-
-//                var outputs = model.forward();
-
-//                var loss = error.calculate(outputs, targets)
-
-//                optimizer.step();
+            if (epoch % 100 == 0) {
+                double lossValue = loss.forward(output, target);
+                System.out.printf("Epoch %d: Loss = %.4f\n", epoch, lossValue);
             }
-
         }
+
+
 
         // for each epoch
         // for each batch
--- src/main/java/org/example/entity/torch/Tensor.java
+++ src/main/java/org/example/entity/torch/Tensor.java
@@ -7,48 +7,57 @@ import java.util.Random;
 
 public class Tensor {
 
-    private final List<List<Double>> tensor;
+    private final double[][] values;
     private final Random random;
 
     public Tensor(List<List<Double>> X) {
-        this.tensor = X;
+        this.values = new double[X.size()][X.get(0).size()];
+        for (int i = 0; i < X.size(); ++ i) {
+            for (int j = 0; j < X.get(0).size(); ++ j) {
+                this.values[i][j] = X.get(i).get(j);
+            }
+        }
         this.random = new Random();
     }
 
     public Tensor(int height, int width, boolean randomInit) {
         if (randomInit) {
-            this.tensor = randInit(height, width);
+            this.values = randInit(height, width);
         } else {
-            this.tensor = initZeros(height, width);
+            this.values = initZeros(height, width);
         }
 
         this.random = new Random();
     }
 
-    private List<List<Double>> initZeros(int height, int width) {
-        List<List<Double>> result = new ArrayList<>();
+    public Tensor(double[][] values) {
+        this.values = values;
+        this.random = new Random();
+    }
+
+    private double[][] initZeros(int height, int width) {
+        var result = new double[height][width];
         for (int i = 0; i < height; ++i) {
-            result.add(new ArrayList<>(Collections.nCopies(width, 0.0)));
+            for (int j = 0; j < width; ++ j) {
+                result[i][j] = 0.0;
+            }
         }
         return result;
     }
 
-    private List<List<Double>> randInit(int height, int width) {
-        var result = new ArrayList<List<Double>>();
+    private double[][] randInit(int height, int width) {
+        var result = new double[height][width];
         for (int i = 0; i < height; ++i) {
-            var temp = new ArrayList<Double>();
             for (int j = 0; j < width; ++j) {
-                var value = random.nextDouble();
-                temp.add(value);
+                result[i][j] = random.nextDouble();
             }
-            result.add(temp);
         }
         return result;
     }
 
     public Shape getShape() {
-        int height = this.tensor.size();
-        int width = (this.tensor.size() > 0) ? this.tensor.get(0).size() : 0;
+        int height = this.values.length;
+        int width = (this.values.length > 0) ? this.values[0].length : 0;
         return new Shape(height, width);
     }
 
@@ -57,14 +66,64 @@ public class Tensor {
      * @return tensor
      */
     public Tensor T() {
-        return null;
+        var shape = this.getShape();
+        Tensor transposed = new Tensor(shape.getWidth(), shape.getHeight(), false);
+        for (int i = 0; i < shape.getWidth(); ++ i) {
+            for (int j = 0; j < shape.getHeight(); ++ j) {
+                transposed.set(i, j, this.values[i][j]);
+            }
+        }
+        return transposed;
     }
 
-    public void add(Tensor bias) {
+    private void set(int i, int j, Double value) {
+        this.values[i][j] = value;
+    }
 
+    /***
+     *
+     * @param bias
+     *  dimension: n x 1
+     *
+     * current tensor size:
+     */
+    public void add(Tensor bias) {
+        Shape shape = bias.getShape();
+
+        int batchSize = shape.getHeight();
+        int outputDim = shape.getWidth();
+        var biasValues = bias.getTensorValues();
+        for (int i = 0; i < batchSize; ++ i) {
+            for (int j = 0; j < outputDim; ++ j) {
+                double value = bias.getTensorValues()[i][j];
+                this.values[i][j] += value;
+            }
+        }
+    }
+
+    private double[][] getTensorValues() {
+        return this.values;
     }
 
     public Tensor multiply(Tensor b) {
-        return null;
+        var input = this.values;
+        var weight = b.getTensorValues();
+
+        int batchSize = input.length;
+        int inFeatures = input[0].length;
+        int outFeatures = weight.length;
+
+        double[][] output = new double[batchSize][outFeatures];
+
+        for (int i = 0; i < batchSize; i++) {
+            for (int j = 0; j < outFeatures; j++) {
+                output[i][j] = 0;
+                for (int k = 0; k < inFeatures; k++) {
+                    output[i][j] += input[i][k] * weight[j][k];
+                }
+            }
+        }
+
+        return new Tensor(output);
     }
 }
