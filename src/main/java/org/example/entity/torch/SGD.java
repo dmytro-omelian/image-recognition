795b0370da69939518141860898fc3a5a173bcda
Dichik <omeluan.dima@gmail.com>
2023-04-30 16:48:29 +0300

    Fix SGD and update that

--- src/main/java/org/example/entity/torch/SGD.java
+++ src/main/java/org/example/entity/torch/SGD.java
@@ -1,5 +1,7 @@
 package org.example.entity.torch;
 
+import java.util.Arrays;
+
 public class SGD {
     private Linear model;
     private double learningRate;
@@ -9,17 +11,25 @@ public class SGD {
         this.learningRate = learningRate;
     }
 
+    public void zero_grad() {
+        for (int i = 0; i < model.gradWeight.length; i++) {
+            for (int j = 0; j < model.gradWeight[0].length; j++) {
+                model.gradWeight[i][j] = 0.0;
+            }
+        }
+
+        Arrays.fill(model.gradBias, 0.0);
+    }
+
     public void step() {
         for (int i = 0; i < model.weight.length; i++) {
             for (int j = 0; j < model.weight[0].length; j++) {
                 model.weight[i][j] -= learningRate * model.gradWeight[i][j];
-                model.gradWeight[i][j] = 0.0;
             }
         }
 
         for (int i = 0; i < model.bias.length; i++) {
             model.bias[i] -= learningRate * model.gradBias[i];
-            model.gradBias[i] = 0.0;
         }
     }
 }
